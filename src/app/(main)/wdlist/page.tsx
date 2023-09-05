5c318c09bf5a0223fbdfde0e3e4a87417c59efa4
taemin-Jang <jtm990506@gmail.com>
2023-09-05 17:54:03 +0900

    Style : JobGruop layout

--- src/app/(main)/wdlist/page.tsx
+++ src/app/(main)/wdlist/page.tsx
@@ -1,3 +1,22 @@
+import ExpandCircleDownOutlinedIcon from '@mui/icons-material/ExpandCircleDownOutlined';
+
 export default function WdList() {
-	return <h1>WdList</h1>;
+	return (
+		<div className='max-w-wanted mt-10 m-auto'>
+			<div className='flex items-center'>
+				<div>
+					<span className='text-2xl font-bold mr-4'>개발</span>
+					<span>
+						<ExpandCircleDownOutlinedIcon fontSize='large' />
+					</span>
+				</div>
+				<div className=' before:content-["|"] before:px-4 before:text-gray-300 before:text-2xl'>
+					<span className='text-2xl mr-4'>개발 전체</span>
+					<span>
+						<ExpandCircleDownOutlinedIcon fontSize='large' />
+					</span>
+				</div>
+			</div>
+		</div>
+	);
 }
