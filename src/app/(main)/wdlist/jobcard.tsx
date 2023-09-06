963a65768a6f311791f356e270a06b1155a51c0d
taemin-Jang <jtm990506@gmail.com>
2023-09-06 23:31:31 +0900

    Feat : Add router to wd/[id]

--- src/app/(main)/wdlist/jobcard.tsx
+++ src/app/(main)/wdlist/jobcard.tsx
@@ -1,11 +1,14 @@
 import type { ProductsAPI, Product } from '@/types/product-api';
 import Loading from '@/app/(main)/wdlist/loading';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 const JobCard = ({ jobList }: { jobList: ProductsAPI | undefined }) => {
+	const router = useRouter();
 	if (!jobList) {
 		return <Loading />;
 	}
+
 	const products: Product[] = jobList.products;
 	console.log(products);
 	return (
@@ -14,7 +17,8 @@ const JobCard = ({ jobList }: { jobList: ProductsAPI | undefined }) => {
 				{products.map((product: Product) => (
 					<div
 						key={product.id}
-						className=' w-[250px] h-96'>
+						className=' w-[250px] h-96 cursor-pointer'
+						onClick={() => router.push(`wd/${product.id}`)}>
 						<Image
 							alt={product.title}
 							src={product.thumbnail}
