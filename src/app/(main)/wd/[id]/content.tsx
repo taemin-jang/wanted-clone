2b32433544a68835af91a55e39400763b5742213
taemin-Jang <jtm990506@gmail.com>
2023-09-25 22:59:50 +0900

    Refactor: wd content component separation

--- src/app/(main)/wd/[id]/content.tsx
+++ src/app/(main)/wd/[id]/content.tsx
@@ -1,29 +1,13 @@
 import Image from 'next/image'
 import Keyword from '@/components/keyword'
-import { JobDetail, Job } from '@/types/wanted-api'
+import { API } from '@/types/wanted-api'
 import ErrorOutlineOutlinedIcon from '@mui/icons-material/ErrorOutlineOutlined'
 import KeyboardArrowDownOutlinedIcon from '@mui/icons-material/KeyboardArrowDownOutlined'
 import dynamic from 'next/dynamic'
 import Loading from '@/app/(main)/wdlist/loading'
-import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined'
-import BookmarkBorderSharpIcon from '@mui/icons-material/BookmarkBorderSharp'
-import FavoriteIcon from '@mui/icons-material/Favorite'
-import Map from '@/app/(main)/wd/[id]/map'
-interface API {
-	newJob: {
-		data: Job[]
-	}
-	jobDetail: {
-		job: JobDetail
-		like_users: LikeUser[]
-	}
-}
 
-interface LikeUser {
-	avatar: string
-	id: number
-	name: string
-}
+import Map from '@/app/(main)/wd/[id]/map'
+import JobPorcess from '@/app/(main)/wd/[id]/jobprocess'
 
 export default function Content({ api }: { api: API }) {
 	const { newJob, jobDetail } = api
@@ -151,80 +135,7 @@ export default function Content({ api }: { api: API }) {
 						</div>
 					</div>
 				</div>
-				<div className='sticky-layout w-[21.25rem] h-full'>
-					<div className=' px-4 py-6  border rounded font-semibold'>
-						<div className='flex justify-between'>
-							<p className='text-sm'>합격보상금</p>
-							<div className='border rounded-full p-1'>
-								<ShareOutlinedIcon color='info' />
-							</div>
-						</div>
-						<div className='w-full flex'>
-							<div className='w-1/2'>
-								<p className='text-sm text-gray-400'>추천인</p>
-								<p className='text-sm mt-2'>
-									{jobDetail.job.reward.formatted_recommendee}
-								</p>
-							</div>
-							<div className='w-1/2'>
-								<p className='text-sm text-gray-400'>지원자</p>
-								<p className='text-sm mt-2'>
-									{jobDetail.job.reward.formatted_recommender}
-								</p>
-							</div>
-						</div>
-						<div className='mt-8'>
-							<div className='flex justify-center border border-blue-500 rounded-full text-blue-500 py-3 font-semibold'>
-								<BookmarkBorderSharpIcon color='info' />
-								<p>북마크하기</p>
-							</div>
-							<div className='flex justify-center bg-blue-600 border rounded-full py-3 mt-2 text-white font-semibold'>
-								지원하기
-							</div>
-						</div>
-						<div className='flex items-center mt-6 '>
-							<div className='border rounded-lg py-2 px-4 mr-6 inline-flex items-center'>
-								<FavoriteIcon
-									sx={{ color: 'lightgray', fontSize: '16px' }}
-									className='mr-1'
-								/>
-								<p className='text-sm'>3</p>
-							</div>
-							<div className='flex relative'>
-								{jobDetail.like_users &&
-									jobDetail.like_users.map((user, index) => (
-										<div
-											key={user.id}
-											className={`-ml-2`}>
-											<Image
-												src={user.avatar}
-												alt={user.name}
-												width={30}
-												height={30}
-												className='border border-white rounded-full'
-											/>
-										</div>
-									))}
-							</div>
-						</div>
-					</div>
-					<div className='border border-blue-100 bg-blue-50 flex items-center justify-between rounded p-4 mt-4'>
-						<div>
-							<p className='font-semibold text-sm'>
-								이 포지션의 면접 예상 질문 확인하기
-							</p>
-							<p className='font-semibold text-sm text-blue-500 mt-2'>
-								AI 면접코칭 받기 {'>'}
-							</p>
-						</div>
-						<Image
-							src='https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Fposition%2Fwanted-plus.png&w=256&q=100'
-							alt='question'
-							width={68}
-							height={68}
-						/>
-					</div>
-				</div>
+				<JobPorcess jobDetail={jobDetail} />
 			</div>
 			<div className='w-[66.25rem] mt-20'>
 				<p className='text-gray-700 font-bold text-lg mb-4'>
---
+++ src/app/(main)/wd/[id]/jobprocess.tsx
@@ -0,0 +1,83 @@
+import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined'
+import BookmarkBorderSharpIcon from '@mui/icons-material/BookmarkBorderSharp'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import Image from 'next/image'
+import type { Detail } from '@/types/wanted-api'
+export default function JobPorcess({ jobDetail }: { jobDetail: Detail }) {
+	return (
+		<div className='sticky-layout w-[21.25rem] h-full'>
+			<div className=' px-4 py-6  border rounded font-semibold'>
+				<div className='flex justify-between'>
+					<p className='text-sm'>합격보상금</p>
+					<div className='border rounded-full p-1'>
+						<ShareOutlinedIcon color='info' />
+					</div>
+				</div>
+				<div className='w-full flex'>
+					<div className='w-1/2'>
+						<p className='text-sm text-gray-400'>추천인</p>
+						<p className='text-sm mt-2'>
+							{jobDetail.job.reward.formatted_recommendee}
+						</p>
+					</div>
+					<div className='w-1/2'>
+						<p className='text-sm text-gray-400'>지원자</p>
+						<p className='text-sm mt-2'>
+							{jobDetail.job.reward.formatted_recommender}
+						</p>
+					</div>
+				</div>
+				<div className='mt-8'>
+					<div className='flex justify-center border border-blue-500 rounded-full text-blue-500 py-3 font-semibold'>
+						<BookmarkBorderSharpIcon color='info' />
+						<p>북마크하기</p>
+					</div>
+					<div className='flex justify-center bg-blue-600 border rounded-full py-3 mt-2 text-white font-semibold'>
+						지원하기
+					</div>
+				</div>
+				<div className='flex items-center mt-6 '>
+					<div className='border rounded-lg py-2 px-4 mr-6 inline-flex items-center'>
+						<FavoriteIcon
+							sx={{ color: 'lightgray', fontSize: '16px' }}
+							className='mr-1'
+						/>
+						<p className='text-sm'>3</p>
+					</div>
+					<div className='flex relative'>
+						{jobDetail.like_users &&
+							jobDetail.like_users.map((user, index) => (
+								<div
+									key={user.id}
+									className={`-ml-2`}>
+									<Image
+										src={user.avatar}
+										alt={user.name}
+										width={30}
+										height={30}
+										className='border border-white rounded-full'
+									/>
+								</div>
+							))}
+					</div>
+				</div>
+			</div>
+			<div className='border border-blue-100 bg-blue-50 flex items-center justify-between rounded p-4 mt-4'>
+				<div>
+					<p className='font-semibold text-sm'>
+						이 포지션의 면접 예상 질문 확인하기
+					</p>
+					<p className='font-semibold text-sm text-blue-500 mt-2'>
+						AI 면접코칭 받기 {'>'}
+					</p>
+				</div>
+				<Image
+					src='https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Fposition%2Fwanted-plus.png&w=256&q=100'
+					alt='question'
+					width={68}
+					height={68}
+				/>
+			</div>
+		</div>
+	)
+}
