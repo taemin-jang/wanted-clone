96654dbb94e3fbcf7dee304cb5a21b1a548043b1
taemin-Jang <jtm990506@gmail.com>
2023-09-26 23:58:17 +0900

    Feat: JobCategory dropdown

--- src/app/(main)/wd/[id]/map.tsx
+++ src/app/(main)/wd/[id]/map.tsx
@@ -1,9 +1,21 @@
-export default function Map({ lng, lat }: { lng: number; lat: number }) {
+export default function Map({
+	address,
+	lng,
+	lat,
+}: {
+	address: string
+	lng: number
+	lat: number
+}) {
 	return (
 		<>
-			<img
-				className='mt-3'
-				src={`https://naveropenapi.apigw.ntruss.com/map-static/v2/raster-cors?w=700&h=254&markers=type:d|color:Red|pos:${lng} ${lat}&X-NCP-APIGW-API-KEY-ID=${process.env.NEXT_PUBLIC_NCP_CLIENT_ID}`}></img>
+			<a
+				href={`https://map.naver.com/?dlevel=13&pinTitle=${address}&lat=${lat}&lng=${lng}`}
+				target='_blank'>
+				<img
+					className='mt-3'
+					src={`https://naveropenapi.apigw.ntruss.com/map-static/v2/raster-cors?w=700&h=254&markers=type:d|color:Red|pos:${lng} ${lat}&X-NCP-APIGW-API-KEY-ID=${process.env.NEXT_PUBLIC_NCP_CLIENT_ID}`}></img>
+			</a>
 		</>
 	)
 }
--- src/app/api/jobs/route.ts
+++ src/app/api/jobs/route.ts
@@ -1,8 +1,10 @@
 import { NextResponse } from 'next/server'
 import { doc, getDoc } from 'firebase/firestore'
 import { db } from '@/middle/firebase'
+import getWanted from '@/utils/getWanted'
 
 export async function GET() {
 	const snapShot = await getDoc(doc(db, 'jobs', 'jobsData'))
+	// const snapShot = await getWanted(`/v4/jobs?country=kr&${params.id}`)
 	return NextResponse.json(snapShot.data())
 }
