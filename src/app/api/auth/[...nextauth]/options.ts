143bc5226477eb2a9500640915ca51e03f84f1c8
taemin-Jang <jtm990506@gmail.com>
2024-04-20 23:33:22 +0900

    Fix: prefix NEXT_PUBLIC_

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -12,9 +12,12 @@ export const options: NextAuthOptions = {
 
 	adapter: FirestoreAdapter({
 		credential: cert({
-			projectId: process.env.FIREBASE_PROJECT_ID,
-			clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-			privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+			projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+			clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
+			privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY?.replace(
+				/\\n/g,
+				'\n',
+			),
 		}),
 	}),
 	providers: [
